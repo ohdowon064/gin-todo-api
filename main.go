b199a18671248af6bdbaaeef97342c8e97948487
Dowon Oh <5d1@hits.ai>
2023-03-12 21:48:42 +0900

    todo 생성 api

--- main.go
+++ main.go
@@ -12,15 +12,21 @@ import (
 type Todo struct {
 	ID          uint `gorm:"primaryKey"`
 	Title       string
-	category    *string   `gorm:"null;default:''"`
-	description *string   `gorm:"null;default:''"`
-	completed   bool      `gorm:"default:false"`
-	createdAt   time.Time `gorm:"autoCreateTime"`
+	category    string
+	description string
+	completed   bool
+	createdAt   time.Time
 	updatedAt   time.Time
 }
 
 type GetTitle struct {
 	Title string `form:"title"`
+}
+
+type CreateTodoRequestBody struct {
+	Title       string `json:"title" binding:"required"`
+	Category    string `json:"category"`
+	Description string `json:"description"`
 }
 
 func main() {
@@ -41,9 +47,23 @@ func main() {
 		c.Bind(&title)
 
 		db.Raw("select * from todos where title ilike ?", "%"+title.Title+"%").Scan(&todos)
-		c.JSON(200, gin.H{
-			"todos": todos,
-		})
+		c.JSON(200, todos)
+	})
+
+	r.POST("/todos", func(c *gin.Context) {
+		var body CreateTodoRequestBody
+		if err := c.Bind(&body); err != nil {
+			fmt.Println(err)
+			c.JSON(400, gin.H{
+				"message": "invalid request body",
+			})
+			return
+		}
+
+		var todo Todo
+		db.Raw("insert into todos (title, category, description) values (?, ?, ?) returning id", body.Title, body.Category, body.Description).Scan(&todo)
+
+		c.JSON(200, todo)
 	})
 
 	r.Run() // listen and serve on 0.0.0.0:8080
