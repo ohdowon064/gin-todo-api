2ee41ff3ba36ac0c6a36542babf05704d148cf24
Dowon Oh <5d1@hits.ai>
2023-03-12 19:49:49 +0900

    Todo struct 선언

--- main.go
+++ main.go
@@ -1,12 +1,45 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"time"
+)
+
+type Todo struct {
+	ID          uint `gorm:"primaryKey"`
+	Title       string
+	category    *string   `gorm:"null;default:''"`
+	description *string   `gorm:"null;default:''"`
+	completed   bool      `gorm:"default:false"`
+	createdAt   time.Time `gorm:"autoCreateTime"`
+	updatedAt   time.Time
+}
+
+type GetTitle struct {
+	Title string `form:"title"`
+}
 
 func main() {
+	dsn := "host=localhost dbname=gin_todo_app port=5432 sslmode=disable TimeZone=Asia/Seoul"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/todos", func(c *gin.Context) {
+		var title GetTitle
+		var todos []Todo
+		c.Bind(&title)
+
+		db.Find(&todos)
+		fmt.Println(todos)
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"todos": todos,
 		})
 	})
 
